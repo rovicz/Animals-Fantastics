8155f0d31f11fff77ff408c687ff5086cebd56af
Victor Fernandes <roviczzf@gmail.com>
2023-03-15 20:24:38 -0300

    final adjustments

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,35 +1,31 @@
 export default function initAnimaNumeros() {
   function animaNumeros() {
     const numeros = document.querySelectorAll('[data-numero]');
-  
+
     numeros.forEach((numero) => {
       const total = +numero.innerText;
-      const incremento = Math.floor(total / 100);
-    
-    
+      const inscremento = Math.floor(total / 100);
       let start = 0;
       const timer = setInterval(() => {
-        start = start + incremento;
+        start += inscremento;
         numero.innerText = start;
-        if(start > total) {
+        if (start > total) {
           numero.innerText = total;
           clearInterval(timer);
         }
       }, 25 * Math.random());
-    })
-    
+    });
   }
-  
+
+  let observer;
   function handleMutation(mutation) {
-    if(mutation[0].target.classList.contains('ativo')) {
+    if (mutation[0].target.classList.contains('ativo')) {
       observer.disconnect();
       animaNumeros();
     }
-    
   }
-  
+  observer = new MutationObserver(handleMutation);
+
   const observerTarget = document.querySelector('.numeros');
-  const observer = new MutationObserver(handleMutation);
-  
-  observer.observe(observerTarget, {attributes: true});
+  observer.observe(observerTarget, { attributes: true });
 }
